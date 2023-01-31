838dfe81db558828e8e2947bddcabc5099e514bf
Alexander <akoceryzhkin@gmail.com>
2023-01-31 16:47:25 +0300

    First Commit

---
+++ main.js
@@ -0,0 +1,133 @@
+'use strict';
+
+Пример №1
+
+let b1 = 5 % 3;
+let b2 = 3 % 5;
+let b3 = 5 + '3';
+let b4 = 75 + 'кг';
+
+console.log(b1);
+console.log(b2);
+console.log(b3);
+console.log(b4);
+
+Пример №2
+
+V = P * dC^2 * heightC
+
+let heightC = 10;
+let dC = 4;
+const P = 3.14;
+let V; 
+V = P * dC ** 2 * heightC;
+console.log(V);
+
+Пример №3
+
+let a = 10;
+let b = 2;
+let c
+let d;
+let e;
+let f;
+c = a + b;
+d = a - b;
+e = a * b;
+f = a / b;
+
+console.log(c);
+console.log(d);
+console.log(e);
+console.log(f);
+
+Пример №4
+
+Первый вариант
+let c = 15;
+let d = 2;
+let result;
+result = c + d;
+console.log(result);
+
+Второй вариант
+let c = 15;
+let d = 2;
+let result = c + d;
+console.log(result);
+
+Пример №5
+
+Первый вариант
+let a = 10, b = 2, c = 5, s = a + b + c;
+console.log(s);
+
+Второй вариант
+let a = 10;
+let b = 2;
+let c = 5;
+let s;
+s = a + b + c;
+console.log(s);
+
+Пример №6
+
+let a = 17;
+let b = 10;
+let c = a - b;
+let d = 7;
+let result = c + d;
+console.log(result);
+
+Пример №7
+
+let name = 'Alexander';
+alert(`Привет, ${name}!`);
+
+Пример №8
+
+let age = 31;
+alert(`Мне ${age} год!`);
+
+Пример №9
+
+let str = 'abcde';
+console.log(str[0]);
+console.log(str[2]);
+console.log(str[4]);
+
+Примет №10
+
+let sec = 1;
+let min = sec * 60;
+let hour = sec * 60 * 60;
+let day = hour * 24;
+let month28 = day * 28;
+let month30 = day * 30;
+let month31 = day * 31;
+console.log(min); 
+console.log(hour);
+console.log(day);
+console.log(month28);
+console.log(month30);
+console.log(month31);
+
+Пример №11
+
+let num = 47;
+num += 7;
+num -= 18;
+num *= 10;
+num /= 15;
+console.log(num);
+
+Пример №12
+
+let num = 10;
+num ++;
+num ++;
+num --;
+console.log(num);
+
+
+
